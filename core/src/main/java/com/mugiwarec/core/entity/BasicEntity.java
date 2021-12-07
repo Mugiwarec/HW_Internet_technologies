23c51b7551dfcdb62d90a36204bfeb71dbcf08ea
Slava <yupigrand@gmail.com>
2021-12-08 02:26:34 +0300

    remove version

--- core/src/main/java/com/mugiwarec/core/entity/BasicEntity.java
+++ core/src/main/java/com/mugiwarec/core/entity/BasicEntity.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
 import java.io.Serializable;
 
 @Data
@@ -16,7 +15,4 @@ public abstract class BasicEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Version
-    private Long version;
 }
